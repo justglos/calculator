dac5107401e22058eb4edf626ec47940c5c25f28
justglos <meras.channel@gmail.com>
2023-03-21 17:22:33 +0200

    calculator

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,17 @@
+a = int(input("Введи первое число"))
+b = int(input("Введи второе число")) 
+c = input("Что будем делать?")
+d = "Ваш ответ: "
+
+if c == "+":
+    print(d, a + b)
+elif c == "-":
+    print(d, a - b)
+elif c == "*":
+    print(d, a * b)
+elif c == "/":
+    print(d, a / b)
+
+
+
+
